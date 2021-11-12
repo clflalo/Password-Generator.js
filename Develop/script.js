bc8e9b0ddf6b815324b3700e609f2f322f4d4a50
Jesus Cifuentes <22laloc@gmail.com>
2021-11-12 13:31:48 -0600

    updated prompts for character selections

--- Develop/script.js
+++ Develop/script.js
@@ -1,16 +1,14 @@
 // Assignment Code
 let generateBtn = document.querySelector("#generate");
 
-//List of Arrays
-let numbers = "0123456789".split("")
-let upperAlphabet = "abcdefghijklmnopqrstuvwxyz".toUpperCase().split("")
-let lowerAlphabet = "abcdefghijklmnopqrstuvwxyz".split("")
-let specialChar = "!@#$%^&*()_+<>?~".split("") 
+//Arrays
+let numbers = "0123456789".split("");
+let upperAlphabet = "abcdefghijklmnopqrstuvwxyz".toUpperCase().split("");
+let lowerAlphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+let specialChar = "!@#$%^&*()_+<>?~".split("") ;
+let arraysSelected = 1;
+let GeneratedPassword = [];
 
-//Selection variables
-let selectLength = "";
-let selectUppercase;
-let selectLowercase;
 
 // Write password to the #password input
 function writePassword() {
@@ -22,18 +20,37 @@ function writePassword() {
 }
 
 function generatePassword(){
-  let selectLength = prompt("Select desired amount of characters for your password");
+  let selectLength = prompt("Enter desired password lenght (8-128 characters)");
 
-// Required characters not met
+// code for if Required # of characters not met
   while(selectLength <= 7 || selectLength >= 128) {
     alert("Password length must meet requirement of 8-128 character length. Try Again.");
     let selectLength = prompt("Select desired amount of characters for your password")
 
   }
 
-  // Password length confirmation
-  alert(`Password will contain ${selectLength} characters`) 
+  // code Password length confirmation
+  alert(`Password will contain ${selectLength} characters`);
 
+  //code Password character types
+
+  let selectUpperCase = prompt("Would you like your password to include uppercase letters? Yes or No?")
+  let selectLowerCase = prompt("Would you like your password to include lowercase letters? Yes or No?")
+  let selectSpecChar = prompt("Would you like your password to include special characters? Yes or no?")
+  let selectNumbers = prompt("Would you like your password to include numbers? Yes or No?")
+
+if (selectUpperCase === "Yes") {
+  arraysSelected++
+}
+console.log(selectUpperCase);
+
+let selectedArrays = "";
+
+for (let i = 0; i < selectLength; i++) {
+arraysSelected = ([Math.floor(Math.random() * arraysSelected)]);
+  
+} 
+return GeneratedPassword.join(""); 
 
 }
 
